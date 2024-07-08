c49df3475c8559cfe65ed6d660ee28f3fc042434
Will Torres <w.torresjr@gmail.com>
2024-07-08 01:03:54 -0400

    Returned animal routes with animal data from query

--- app/api/animal_routes.py
+++ app/api/animal_routes.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Rescue
+from app.models import Rescue, db
 
 
 animal_routes = Blueprint("animals", __name__)
 
 
-@animal_routes.route("/")
+@animal_routes.route("/", methods=["GET"])
 def get_all_animals():
-    return jsonify({"Success":"Route Reached"})
+    
+    
+    animals_found = Rescue.query.all()
+    
+    animals = [animal.to_dict() for animal in animals_found]
+      
+    return animals
