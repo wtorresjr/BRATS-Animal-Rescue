43d4f665a3502097ca1ac69aeac4720898f5528b
Will Torres <w.torresjr@gmail.com>
2024-07-08 17:46:31 -0400

    Created delete animal route

--- app/api/animal_routes.py
+++ app/api/animal_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import Rescue, db
 
@@ -20,11 +20,32 @@ def get_all_animals():
 
 
 
+@animal_routes.route("/<int:animal_id>", methods=["PUT","DELETE"])
+def animal_operations_by_id(animal_id):
+    
+    animal_found = Rescue.query.filter_by(id=animal_id).first()
+    
+    if not animal_found:
+        return jsonify({"message":f"Error: Animal ID {animal_id} cannot be found."})
+    
+    if request.method == "PUT":
+        return jsonify({"message":"EDIT ROUTE REACHED"})
+ 
+    if request.method == "DELETE":
+        db.session.delete(animal_found)
+        db.session.commit()
+        
+        return jsonify({"message":f"Animal ID {animal_id} was successfully deleted."})
+
+
+
 @animal_routes.route("/<int:animal_id>", methods=["GET"])
 def get_animal_by_id(animal_id):
+    animal_found = Rescue.query.filter_by(id=animal_id).first()
     
-    animal_found = Rescue.query.filter_by(id=animal_id).all()
+    if not animal_found:
+        return jsonify({"message":f"Error: Animal ID {animal_id} cannot be found."})
     
-    result = [animal.to_dict() for animal in animal_found]
+    return animal_found.to_dict()
     
-    return result
+    
