48b2af284623bd9344005946a0e254784f5dcf72
Will Torres <w.torresjr@gmail.com>
2024-07-08 01:39:09 -0400

    Get animal by ID route created

--- app/api/animal_routes.py
+++ app/api/animal_routes.py
@@ -9,9 +9,22 @@ animal_routes = Blueprint("animals", __name__)
 @animal_routes.route("/", methods=["GET"])
 def get_all_animals():
     
-    
     animals_found = Rescue.query.all()
+    
+    if not animals_found:
+        return jsonify({"message":"No animals found"})
     
     animals = [animal.to_dict() for animal in animals_found]
       
     return animals
+
+
+
+@animal_routes.route("/<int:animal_id>", methods=["GET"])
+def get_animal_by_id(animal_id):
+    
+    animal_found = Rescue.query.filter_by(id=animal_id).all()
+    
+    result = [animal.to_dict() for animal in animal_found]
+    
+    return result
