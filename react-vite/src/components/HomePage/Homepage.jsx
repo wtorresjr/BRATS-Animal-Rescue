5997384b89fb3a0c3eb597c2f6dde6f13abf2bf1
Will Torres <w.torresjr@gmail.com>
2024-07-09 20:55:09 -0400

    Updated login logic for admin, updated routing

--- react-vite/src/components/HomePage/Homepage.jsx
+++ react-vite/src/components/HomePage/Homepage.jsx
@@ -1,14 +1,9 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
-import { getAllAnimalsThunk } from "../../redux/animals";
 import AnimalCard from "../AnimalCard/AnimalCard";
 
 const Homepage = () => {
   const { animals, loading, error } = useContext(AppContext);
-
-  // const currRescues = animals["animals"];
-
-  // console.log(...currRescues);
 
   if (loading) {
     <div>LOADING...</div>;
