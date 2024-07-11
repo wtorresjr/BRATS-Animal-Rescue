015b3553ae41cde26f62fe3a2d582d35d57940dc
Will Torres <w.torresjr@gmail.com>
2024-07-11 00:10:51 -0400

    Added validation feature

--- react-vite/src/components/AdminPage/AdminPage.jsx
+++ react-vite/src/components/AdminPage/AdminPage.jsx
@@ -1,5 +1,221 @@
+import { useEffect, useState } from "react";
+import { addAnimalThunk } from "../../redux/animals";
+import { useDispatch } from "react-redux";
+import "./admin-page.css";
+import validateData from "./validation";
+
 const AdminPage = () => {
-  return <>Admin Page</>;
+  const dispatch = useDispatch();
+
+  const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [fixed, setFixed] = useState("");
+  const [gwCats, setGWCats] = useState("");
+  const [gwDogs, setGWDogs] = useState("");
+  const [gwKids, setGWKids] = useState("");
+  const [trained, setTrained] = useState("");
+  const [rescueDate, setRescueDate] = useState("");
+  const [sex, setSex] = useState("");
+  const [story, setStory] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+  const [rescueType, setRescueType] = useState("");
+
+  const [formErrors, setFormErrors] = useState({});
+
+  useEffect(() => {
+    console.log(formErrors, "Errors from useeffect");
+  }, [formErrors]);
+
+  const addAnimal = async () => {
+    const data = {
+      animal_name: name,
+      age: parseInt(age),
+      breed: breed,
+      fixed: parseInt(fixed),
+      good_w_cats: parseInt(gwCats),
+      good_w_dogs: parseInt(gwDogs),
+      good_w_kids: parseInt(gwKids),
+      potty_trained: parseInt(trained),
+      rescue_date: rescueDate,
+      sex: sex,
+      story: story,
+      thumbnail_img: thumbnail,
+      type: rescueType,
+    };
+
+    const validationResult = await validateData(data);
+
+    if (validationResult === true) {
+      try {
+        const addRescue = await dispatch(addAnimalThunk(data));
+        if (addRescue) {
+          alert("Added Rescue Successfully!");
+        }
+      } catch (error) {
+        console.error("Error Adding Rescue", error);
+      }
+    } else {
+      // console.log(validationResult, "ERRORS from Return");
+      setFormErrors(validationResult);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {formErrors.name_error && (
+          <span className="errors-red">{formErrors.name_error}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Age:</label>
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        {formErrors.age_error && (
+          <span className="errors-red">{formErrors.age_error}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Rescue Type:</label>
+        <select
+          value={rescueType}
+          onChange={(e) => setRescueType(e.target.value)}
+        >
+          <option value={"Dog"}>Dog</option>
+          <option value={"Cat"}>Cat</option>
+        </select>
+        {formErrors.rescueType && (
+          <span className="errors-red">{formErrors.rescueType}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Breed:</label>
+        <input
+          type="text"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        />
+        {formErrors.breed_error && (
+          <span className="errors-red">{formErrors.breed_error}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Fixed:</label>
+        <select value={fixed} onChange={(e) => setFixed(e.target.value)}>
+          <option value={0}>No</option>
+          <option value={1}>Yes</option>
+        </select>
+        {formErrors.fixed_error && (
+          <span className="errors-red">{formErrors.fixed_error}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Good With Cats?</label>
+        <select value={gwCats} onChange={(e) => setGWCats(e.target.value)}>
+          <option value={0}>No</option>
+          <option value={1}>Yes</option>
+        </select>
+        {formErrors.good_w_cats && (
+          <span className="errors-red">{formErrors.good_w_cats}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Good With Dogs?</label>
+        <select value={gwDogs} onChange={(e) => setGWDogs(e.target.value)}>
+          <option value={0}>No</option>
+          <option value={1}>Yes</option>
+        </select>
+        {formErrors.good_w_dogs && (
+          <span className="errors-red">{formErrors.good_w_dogs}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Good With Kids?</label>
+        <select value={gwKids} onChange={(e) => setGWKids(e.target.value)}>
+          <option value={0}>No</option>
+          <option value={1}>Yes</option>
+        </select>
+        {formErrors.good_w_kids && (
+          <span className="errors-red">{formErrors.good_w_kids}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Potty Trained?</label>
+        <select value={trained} onChange={(e) => setTrained(e.target.value)}>
+          <option value={0}>No</option>
+          <option value={1}>Yes</option>
+        </select>
+        {formErrors.potty_trained && (
+          <span className="errors-red">{formErrors.potty_trained}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Rescue Date:</label>
+        <input
+          type="date"
+          value={rescueDate}
+          onChange={(e) => setRescueDate(e.target.value)}
+        />
+        {formErrors.rescue_date && (
+          <span className="errors-red">{formErrors.rescue_date}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Sex:</label>
+        <select value={sex} onChange={(e) => setSex(e.target.value)}>
+          <option value={"Male"}>Male</option>
+          <option value={"Female"}>Female</option>
+        </select>
+        {formErrors.sex && <span className="errors-red">{formErrors.sex}</span>}
+      </div>
+
+      <div>
+        <label>Story:</label>
+        <input
+          type="textarea"
+          value={story}
+          onChange={(e) => setStory(e.target.value)}
+        />
+        {formErrors.story && (
+          <span className="errors-red">{formErrors.story}</span>
+        )}
+      </div>
+
+      <div>
+        <label>Thumbnail Image URL:</label>
+        <input
+          type="text"
+          value={thumbnail}
+          onChange={(e) => setThumbnail(e.target.value)}
+        />
+        {formErrors.thumbnail_img && (
+          <span className="errors-red">{formErrors.thumbnail_img}</span>
+        )}
+      </div>
+
+      <button onClick={addAnimal}>Add New Rescue</button>
+    </>
+  );
 };
 
 export default AdminPage;
