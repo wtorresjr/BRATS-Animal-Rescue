5911a6c59842a54959f1fe30d2029ea3f1e4aa96
Will Torres <w.torresjr@gmail.com>
2024-07-16 01:06:15 -0400

    Added menu

---
+++ react-vite/src/components/NavMenu/NavMenu.jsx
@@ -0,0 +1,16 @@
+import "./navmenu.css";
+
+const NavMenu = () => {
+  return (
+    <div className="navmenu">
+      <button>Foster A Rescue</button>
+      <button>Adopt A Rescue</button>
+      <button>Event Calendar</button>
+      <button>Make A Donation</button>
+      <button>Our Sponsors</button>
+      <button>Ways To Volunteer</button>
+    </div>
+  );
+};
+
+export default NavMenu;
