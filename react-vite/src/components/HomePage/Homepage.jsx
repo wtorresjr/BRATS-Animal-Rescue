9073f70951eb2ed6d216e5f298c254aeb57ba198
Will Torres <w.torresjr@gmail.com>
2024-07-17 01:35:02 -0400

    started creation of adopt card

--- react-vite/src/components/HomePage/Homepage.jsx
+++ react-vite/src/components/HomePage/Homepage.jsx
@@ -3,13 +3,32 @@ import { Header } from "../Header";
 import { NavMenu } from "../NavMenu";
 import { MyContext } from "../../context/MyContext";
 import { useContext } from "react";
+import { Button } from "@mui/material";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import AdoptCard from "../AdoptCard/AdoptCard";
 
 const Homepage = () => {
   const { openMenu, setOpenMenu } = useContext(MyContext);
   return (
     <>
       <Header />
+
       {openMenu ? <NavMenu /> : null}
+
+      <div style={{ margin: "10px 0 0 0" }}></div>
+
+      <Button
+        style={{ backgroundColor: "#ca0300", alignItems: "center" }}
+        fullWidth={"True"}
+        variant="contained"
+        endIcon={<ArrowRightIcon />}
+      >
+        Our Mission
+      </Button>
+
+      <div style={{ margin: "10px 0 0 0" }}></div>
+
+      <AdoptCard />
     </>
   );
 };
