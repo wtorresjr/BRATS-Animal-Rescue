d3e00e02efea06825c291dee9d96359f14be58f6
Will Torres <w.torresjr@gmail.com>
2024-07-16 23:03:17 -0400

    Added material ui and created state for nav menu button

--- react-vite/src/components/Header/Header.jsx
+++ react-vite/src/components/Header/Header.jsx
@@ -1,12 +1,33 @@
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 import "./header.css";
 
 const logo = "../src/Assets/Brats-Logo-simple.png";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="header">
       <img src={logo} />
       <p className="header-title">Bringing Rescued Animals To Safety</p>
+      <Button
+        onClick={() => setMenuOpen(!menuOpen)}
+        variant="outlined"
+        style={{
+          color: "#ca0300",
+          border: "2px solid #ca0300",
+          borderRadius: "10px",
+        }}
+      >
+        {menuOpen ? (
+          <HighlightOffIcon fontSize={"large"} />
+        ) : (
+          <MenuRoundedIcon fontSize={"large"} />
+        )}
+      </Button>
     </div>
   );
 };
