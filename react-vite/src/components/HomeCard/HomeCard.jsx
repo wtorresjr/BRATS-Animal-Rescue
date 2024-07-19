6180d936e4ccbe1d424a1a67fb46f06a3452c747
Will Torres <w.torresjr@gmail.com>
2024-07-18 23:48:34 -0400

    Fixed ratio for img on home cards

--- react-vite/src/components/HomeCard/HomeCard.jsx
+++ react-vite/src/components/HomeCard/HomeCard.jsx
@@ -1,6 +1,8 @@
 import "./homecard.css";
 
 const HomeCard = ({ title, cardText, callToAction, imgSrc, cssStyle }) => {
+  const [title1, title2] = title.split(" ");
+
   return (
     <div className={`${cssStyle}-card`}>
       <div className={`${cssStyle}-img`}>
@@ -8,9 +10,10 @@ const HomeCard = ({ title, cardText, callToAction, imgSrc, cssStyle }) => {
       </div>
       <div className={`${cssStyle}-text`}>
         <div className={`${cssStyle}-text-title`}>
-          <h1>{title}</h1>
-          {cardText}
+          {title1}
+          <div className="regular-weight"> {title2}</div>
         </div>
+        {cardText}
         <h4>{callToAction}</h4>
       </div>
     </div>
