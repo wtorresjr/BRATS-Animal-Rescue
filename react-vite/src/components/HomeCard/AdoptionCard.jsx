2f7c36a42a22c3c34b467850185e329f222833f3
Will Torres <w.torresjr@gmail.com>
2024-07-25 21:48:38 -0400

    Created adopt card detail component

---
+++ react-vite/src/components/HomeCard/AdoptionCard.jsx
@@ -0,0 +1,13 @@
+const AdoptionCard = ({cssStyle}) => {
+  return (
+    <div className={`${cssStyle}-card`}>
+      <div className={`${cssStyle}-img`}>
+        <img src=""></img>
+      </div>
+      <div className={`${cssStyle}-text`}>
+      </div>
+    </div>
+  );
+};
+
+export default AdoptionCard;
