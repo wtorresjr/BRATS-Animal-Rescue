b7c633b0e9c48231648fc5e1721c15d9f5892fac
Will Torres <w.torresjr@gmail.com>
2024-07-31 00:21:56 -0400

    Update build script

--- react-vite/src/components/HomeCard/AdoptionCard.jsx
+++ react-vite/src/components/HomeCard/AdoptionCard.jsx
@@ -2,7 +2,7 @@ const AdoptionCard = ({ cssStyle }) => {
   return (
     <div className={`${cssStyle}-card`}>
       <div className={`${cssStyle}-img`}>
-        <img src="../../public/imgs/dog-img1.jpg"></img>Test
+        <img src="../src/Assets/dog-img1.jpg"></img>Test
       </div>
     </div>
   );
