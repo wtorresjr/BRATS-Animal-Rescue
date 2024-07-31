866c9f350dfec26caae63705debb33998fc131d5
Will Torres <w.torresjr@gmail.com>
2024-07-31 00:04:59 -0400

    Update assets location

--- react-vite/src/components/HomeCard/AdoptionCard.jsx
+++ react-vite/src/components/HomeCard/AdoptionCard.jsx
@@ -1,8 +1,8 @@
 const AdoptionCard = ({ cssStyle }) => {
   return (
-      <div className={`${cssStyle}-card`}>
+    <div className={`${cssStyle}-card`}>
       <div className={`${cssStyle}-img`}>
-        <img src="../src/Assets/dog-img1.jpg"></img>Test
+        <img src="../../public/imgs/dog-img1.jpg"></img>Test
       </div>
     </div>
   );
