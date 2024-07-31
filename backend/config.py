5bd85d62722a8a719fe40d6543ea5340e17f8103
Will Torres <w.torresjr@gmail.com>
2024-07-30 23:18:05 -0400

    Update db url 2

--- backend/config.py
+++ backend/config.py
@@ -10,12 +10,12 @@ class Config:
     FLASK_RUN_PORT = os.environ.get("FLASK_RUN_PORT")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    #     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-    #         "postgres://", "postgresql://", 1)
-
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1)
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     SQLALCHEMY_ECHO = True
     print(SQLALCHEMY_DATABASE_URI, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
