ec36a67ba1d016322be3d790b83f52bc1e788a68
wtorresjr <w.torresjr@gmail.com>
2024-08-04 18:28:58 -0400

    General styling of adoption page

--- react-vite/src/components/HomeCard/AdoptionCard.jsx
+++ react-vite/src/components/HomeCard/AdoptionCard.jsx
@@ -1,9 +1,27 @@
-const AdoptionCard = ({ cssStyle }) => {
+const AdoptionCard = ({ cssStyle, rescue }) => {
+  console.log(rescue);
+  const r_date = rescue.rescue_date.split(" ");
+
   return (
     <div className={`${cssStyle}-card`}>
       <div className={`${cssStyle}-img`}>
-        <img src="dog-img1.jpg"></img>Test
+        <img src="dog-img1.jpg"></img>
       </div>
+
+      <div className={`${cssStyle}-adptText`}>
+        <h1>{rescue.animal_name}</h1>
+        <div>{`Rescue Date: ${r_date[2]}-${r_date[1]}-${r_date[3]}`}</div>
+        <div>Age: {rescue.age}</div>
+        <div>Sex: {rescue.sex}</div>
+        <div>Good With Kids? {rescue.good_w_kids ? "Yes" : "No"}</div>
+        <div>Good With Cats? {rescue.good_w_cats ? "Yes" : "No"}</div>
+        <div>Good With Dogs? {rescue.good_w_dogs ? "Yes" : "No"}</div>
+        <div>Spayed/Neutered? {rescue.fixed ? "Yes" : "No"}</div>
+        <div>Potty Trained? {rescue.potty_trained ? "Yes" : "No"}</div>
+      </div>
+        <div>
+          {`${rescue.animal_name}'s Story`}: {rescue.story}
+        </div>
     </div>
   );
 };
